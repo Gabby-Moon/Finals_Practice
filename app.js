5e394a3f0e36153f8697c4b9bdad1f4795e56b82
Gabby-Moon <194023457+Gabby-Moon@users.noreply.github.com>
2025-03-12 11:59:58 -0700

    form is now on home page and start of getting variables from form in app.js

--- app.js
+++ app.js
@@ -6,15 +6,20 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 const PORT = 3000;
+const workouts = [];
 
 app.get('/', (req, res) => {
     res.render('home');
 });
 
 app.post('/summary', (req, res) => {
-    res.render('summary');
+    const workout = {
+
+    };
+    workouts.push(workout);
+    res.render('summary', workouts);
 });
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
-})
+});
