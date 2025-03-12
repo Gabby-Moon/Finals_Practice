a1984e6cbc6a5402a8715726a8e3e5b27067aaca
Gabby-Moon <194023457+Gabby-Moon@users.noreply.github.com>
2025-03-12 14:58:19 -0700

    added comments to code

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 import express from 'express';
 
+// make app
 const app = express();
 app.use(express.urlencoded({ extended: true }));
+// access ejs files
 app.set('view engine', 'ejs');
+// set where it finds files
 app.use(express.static('public'));
 
 const PORT = 3000;
 const workouts = [];
 
+// home route
 app.get('/', (req, res) => {
     res.render('home');
 });
 
+// summary route after form is submitted
 app.post('/summary', (req, res) => {
+    // taking information from the form
     const workout = {
         workoutType: req.body.workoutType,
         duration: req.body.duration,
@@ -21,10 +27,13 @@ app.post('/summary', (req, res) => {
         notes: req.body.notes
     };
     workouts.push(workout);
+    // test
     console.log(workouts);
+    // pass it to page
     res.render('summary', { workouts });
 });
 
+// listening to port for page to run locally
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
--- public/scripts/buttons.js
+++ public/scripts/buttons.js
@@ -1,3 +1,4 @@
+// gets the button on the summary page to go back to the home directory
 document.getElementById('to-home').onclick = function toHome () {
     window.location.href = '/';
 };
